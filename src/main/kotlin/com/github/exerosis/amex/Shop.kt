21bc7ad815c4467fb835909e366fec2fb24d09be
TheExerosisGaming <theexerosisgaming@gmail.com>
2020-11-08 04:20:34 -0500

    ~ Step 2 complete.

--- src/main/kotlin/com/github/exerosis/amex/Shop.kt
+++ src/main/kotlin/com/github/exerosis/amex/Shop.kt
@@ -5,19 +5,20 @@ import java.text.NumberFormat
 
 
 val FORMAT_CURRENCY = NumberFormat.getCurrencyInstance()!!
+//discounts can actually manage to be implemented in these functors.
 val PRICES = mapOf<String, (Int) -> (Double)>(
-    "apple" to { it * 0.60 },
-    "orange" to { it * 0.25 }
+    "apple" to { it / 2 * 0.60 + it % 2 * 0.60 },
+    "orange" to { it / 3 * 0.50 + it % 3 * 0.25 }
 )
-//map orders to responses.
+//map orders to responses. (using caps to represent "class like" functionality)
 fun shop(onOrder: Event<(Sequence<String>) -> (Unit)>): Event<(String) -> (Unit)> = { listener ->
-   onOrder { items ->
-       listener(try { //FIXME if given more time, perhaps don't use try as logic.
-           //group by the price functor then sum by that functor and group count
-           FORMAT_CURRENCY.format(items.groupBy {
-               PRICES[it.toLowerCase()] ?: throw Exception("Could not find item: $it")
-           }.asSequence().sumByDouble { (price, group) -> price(group.size) })
-           //alternatively something failed (likely finding the item)
-       } catch (reason: Throwable) { reason.message ?: "failed" })
-   }
+    onOrder { items ->
+        listener(try {
+            //group by the price functor then sum by that functor and group count
+            FORMAT_CURRENCY.format(items.groupBy {
+                PRICES[it.toLowerCase()] ?: throw Exception("Could not find item: $it")
+            }.asSequence().sumByDouble { (price, group) -> price(group.size) })
+            //alternatively something failed (likely finding the item)
+        } catch (reason: Throwable) { reason.message ?: "failed" })
+    }
 }
